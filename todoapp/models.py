b4485b37ec3df63cfe49e547cb37cd2126b77a7e
siva0812 <63805971+siva0812@users.noreply.github.com>
2020-10-01 13:49:24 +0530

    task2
    todo list app

---
+++ todoapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here..
+class Todo(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    priority = models.IntegerField()
+    
---
+++ todoapp/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from  .models import *
+from django.http import HttpResponse,JsonResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def submit(request):
+    obj = Todo()
+    obj.title = request.GET['title']
+    obj.description = request.GET['description']
+    obj.priority = request.GET['priority']
+    obj.save()
+    mydictionary = {
+        "alltodos" : Todo.objects.all()
+    }
+    return render(request,'list.html',context=mydictionary)
+
+def delete(request,id):
+    obj = Todo.objects.get(id=id)
+    obj.delete()
+    mydictionary = {
+        "alltodos" : Todo.objects.all()
+    }
+    return render(request,'list.html',context=mydictionary)
+
+
+def list(request):
+    mydictionary = {
+        "alltodos" : Todo.objects.all()
+    }
+    return render(request,'list.html',context=mydictionary)
+
+
+def edit(request,id):
+    obj = Todo.objects.get(id=id)
+    mydictionary = {
+        "title" : obj.title,
+        "description" : obj.description,
+        "priority" : obj.priority,
+        "id" : obj.id
+    }
+    return render(request,'edit.html',context=mydictionary)
+
+
+def update(request,id):
+    obj = Todo(id=id)
+    obj.title = request.GET['title']
+    obj.description = request.GET['description']
+    obj.priority = request.GET['priority']
+    obj.save()
+    mydictionary = {
+        "alltodos" : Todo.objects.all()
+    }
+    return render(request,'list.html',context=mydictionary)
